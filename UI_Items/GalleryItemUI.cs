6c11cc1db074833c29424f3d90446b6775abd56a
Umair M <umair.em@outlook.com>
2016-08-15 11:17:55 +0100

    Add files via upload

---
+++ UI_Items/GalleryItemUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GalleryItemUI : MonoBehaviour 
+{
+	
+	public string filePath;
+	public Texture2D thmbnail;
+	public Text title;
+	void Start()
+	{
+		title.text = System.IO.Path.GetFileName(filePath);
+	}
+
+	public void OnClicked()
+	{
+		DataManager.Instance.CurrentVideoPath = filePath;
+		MenuManager.Instance.CurrentMenuState = MenuStates.PreviewMenu;
+	}
+}
