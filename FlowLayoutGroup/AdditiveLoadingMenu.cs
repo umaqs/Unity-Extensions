1be9143a97f75c58d87273bef35f09a38500e9be
Umair M <umair.em@outlook.com>
2016-08-15 11:20:51 +0100

    Add files via upload

---
+++ FlowLayoutGroup/AdditiveLoadingMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+ class AdditiveLoadingMenu  : MonoBehaviour
+{
+
+	public SceneLoader sceneLoader;
+    public Slider progressBar;
+
+	void Start()
+    {
+        if (progressBar == null)
+        {
+            Debug.LogError(" AdditiveLoadingMenu : ProgressBar is null");
+        }
+    }
+
+
+    void Update()
+    {
+		if (progressBar != null && sceneLoader != null)
+        {
+			progressBar.value = sceneLoader.loader.progress;
+        }
+
+    }
+ }
